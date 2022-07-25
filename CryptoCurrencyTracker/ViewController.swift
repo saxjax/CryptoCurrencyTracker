b38780ac3e9a7f2b7cb0ee67e344aa4daf81968e
saxjax <saxjax@saxjax.dk>
2022-07-25 20:37:51 +0300

    Initial Commit

---
+++ CryptoCurrencyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCurrencyTracker
+//
+//  Created by Jakob Skov Søndergård on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
